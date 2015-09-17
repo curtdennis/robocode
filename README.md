a9665227ee32bcadf839a863c433ee9d76b2d2be
Curt Dennis <curt@curtdennis.com>
2015-09-16 22:31:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Synopsis
+
+Robocode is a platform for creating code-driven battlebots.  Your mission is to create an artifically intelligent robot bent on destruction.  After designing your robot to fight against robot enemies, you will then face off against each other in what will certainly be a battle where only one will survive.  
+
+While there are MANY strategies you can employ to confuse or dispose of the competition, you have to decide if brains or brawn will win the day.  
+
+## Where do I sign up!?
+
+Clone this repository to your local machine and open the Eclipse project.  In this project you can begin writing your own personal robot intelligence.  The main file you will work in is XX.
+
+## How to I run my bot?
+
+Execute
+
+## Links
+
+http://robowiki.net/wiki/Robocode_Download_And_Install
---
+++ robocode_robot/src/yourteamname/YourTeamName.java
@@ -0,0 +1,57 @@
+package yourteamname;
+import robocode.*;
+import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * YourTeamName - Rename this class to your robot's name!  Don't forget the file too :)
+ */
+public class YourTeamName extends Robot
+{
+	/**
+	 * run: default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
